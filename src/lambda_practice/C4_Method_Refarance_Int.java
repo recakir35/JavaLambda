6877226ea792ede9f32a0b769c8f89964c4ea6a7
Recep Çakır <recakir35@gmail.com>
2022-05-13 21:52:45 +0300

    13.05.2022

--- src/lambda_practice/C4_Method_Refarance_Int.java
+++ src/lambda_practice/C4_Method_Refarance_Int.java
@@ -15,7 +15,6 @@ public class C4_Method_Refarance_Int {
     public static void main(String[] args) {
         List<Integer> intL= Arrays.asList(-2,-8,-5,0,2,5,6,7,15,6,8);
 
-
     }
     // S1: tekleri aralarinda bir bosluk birakarak yazdiralim
 
